c19809624d9bdd9533c858cc79659c015bb1379b
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-01-21 10:11:07 -0300

    feat: implements Absinthe Schema types

--- lib/cumbuca/graphql/schema.ex
+++ lib/cumbuca/graphql/schema.ex
@@ -1,16 +1,9 @@
 defmodule Cumbuca.Graphql.Schema do
   use Absinthe.Schema
+  import_types Cumbuca.Graphql.Types
 
   alias Cumbuca.Graphql.Resolver.ChargeResolver
-
-  object :user do
-    field :id, non_null(:id)
-    field :name, non_null(:string)
-    field :cpf, non_null(:integer)
-    field :email, non_null(:string)
-    field :balance, non_null(:integer)
-  end
-
+  
   query do 
     field :all_users, non_null(list_of(non_null(:user))) do 
       resolve(&ChargeResolver.all_users/3)
---
+++ lib/cumbuca/graphql/types.ex
@@ -0,0 +1,12 @@
+defmodule Cumbuca.Graphql.Types do
+  use Absinthe.Schema.Notation
+
+@desc "Conta de usuario"
+object :user do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :cpf, non_null(:integer)
+    field :email, non_null(:string)
+    field :balance, non_null(:integer)
+  end
+end
